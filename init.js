7f38823099a7c6cf7e6d586ab548d10d19b85ced
sdcomms4227 <sdcomms4227@gmail.com>
2020-10-30 12:30:37 +0900

    complete #6 USER AUTHENTICATION LIKE A BOSS

--- init.js
+++ init.js
@@ -11,6 +11,6 @@ import "./models/User";
 const PORT = process.env.PORT || 4000;
 
 const handleListening = () =>
-  console.log(`Listening on http://localhost:${PORT}`);
+  console.log(`✔ Listening on http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
--- passport.js
+++ passport.js
@@ -27,7 +27,9 @@ passport.use(
     {
       clientID: process.env.FB_ID,
       clientSecret: process.env.FB_SECRET,
-      callbackURL: `http://localhost:4000${routes.facebookCallback}`,
+      callbackURL: `https://32b64d0efda5.ngrok.io${routes.facebookCallback}`,
+      profileFields: ["id", "displayName", "photos", "email"],
+      scope: ["public_profile", "email"],
     },
     facebookLoginCallback
   )
